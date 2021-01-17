b136bccfeda02f9aa5190def093b491c912fa47f
Godwinpy <godwinayeni@gmail.com>
2021-01-16 19:15:55 -0600

    First Commit

---
+++ Lab3.py
@@ -0,0 +1,10 @@
+print(24 + 25)
+print(23 * 12)
+print(10 + 0.5)
+Price = 40
+print(Price + 5)
+
+Sales = 5
+print(Price-Sales)
+print("The original price was" + str(40)+ ". With a sale of " + str(0.15) + ", your current price is " + str(34))
+print("$ " + str(34))
