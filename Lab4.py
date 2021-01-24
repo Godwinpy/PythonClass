b6ae5f12f2b01ebe6ee2d5ad4a20824ba0cf1424
Godwinpy <godwinayeni@gmail.com>
2021-01-23 18:56:40 -0600

    [Godwin] - Commit lab4

---
+++ Lab4.py
@@ -0,0 +1,12 @@
+
+
+Songs = ["Victroy" , "Blessed", "Elshadia", "Lifted", "Hossanah", "Immortal" , "Alabastar" , "Come", "Salavation" , "Almighty"]
+print(Songs)
+
+
+cloth_dict = {"Sport": "yellow", "traditional" : "white" , "officiaL" : "Pink"}
+
+print(cloth_dict["Sport"])
+
+set_of_fruits = set('appleorangeblack')
+print(set_of_fruits)
